13d4795c8e434be5d8e8a5a33a3b1a8b11ff9b2f
dalinwanglhtz <dalinwanglhtz@gmail.com>
2022-08-14 22:22:09 +1000

    Add table for results

--- force-app/main/default/lwc/searchSource/searchSource.js
+++ force-app/main/default/lwc/searchSource/searchSource.js
@@ -1,13 +1,20 @@
 import { LightningElement } from 'lwc';
 import getApexClass from '@salesforce/apex/SearchSourceController.getApexClass';
 
+const columns = [
+    { label: 'Class Name', fieldName: 'Name' },
+    { label: 'Source', fieldName: 'Body' }
+];
 export default class SearchSource extends LightningElement {
+    data = [];
+    columns = columns;
 
     handleChange(event) {
         console.log('Text: ', event.target.value);
         getApexClass({searchKey: event.target.value})
             .then(result => {
                 console.log('Result: ', result[0]);
+                this.data = result;
             })
             .catch(error => {
                 console.log('Error: ', error.body.message);
