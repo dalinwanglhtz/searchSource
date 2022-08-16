576f9d4321f05f7ccc71952ad73095ab2f7b68f6
dalinwanglhtz <dalinwanglhtz@gmail.com>
2022-08-16 19:37:32 +1000

    Add action to show detail

--- force-app/main/default/lwc/searchSource/searchSource.js
+++ force-app/main/default/lwc/searchSource/searchSource.js
@@ -1,23 +1,60 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getApexClass from '@salesforce/apex/SearchSourceController.getApexClass';
+
+const actions = [
+    { label: 'Show Details', name: 'showDetails'}
+]
 
 const columns = [
     { label: 'Class Name', fieldName: 'Name' },
-    { label: 'Source', fieldName: 'Body' }
+    { type: 'action', 
+        typeAttributes: {
+            rowActions: actions
+        }
+    }
 ];
-export default class SearchSource extends LightningElement {
+export default class SearchSource extends NavigationMixin(LightningElement) {
     data = [];
     columns = columns;
+    err;
 
     handleChange(event) {
-        console.log('Text: ', event.target.value);
-        getApexClass({searchKey: event.target.value})
+        const word = event.target.value;
+        if(!word) {
+            this.data = [];
+            return;
+        }
+
+        getApexClass({searchKey: word})
             .then(result => {
-                console.log('Result: ', result[0]);
                 this.data = result;
             })
             .catch(error => {
-                console.log('Error: ', error.body.message);
+                this.err = error;
             });
+    }
+
+    handleRowAction(event) {
+        const action = event.detail.action;
+        const row = event.detail.row;
+        switch(action.name) {
+            case 'showDetails':
+                this.navigateToWebPage(row.Id);
+                break;
+        }
+    }
+
+    navigateToWebPage(rowId) {
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: rowId,
+                objectApiName: 'ApexClass',
+                actionName: 'view'
+            }
+        }).then(url => {
+            window.open(url, '_blank');
+        });
     }
 }
